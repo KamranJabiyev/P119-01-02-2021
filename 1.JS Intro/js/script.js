631d7feb79118402cb2486a01a9adfa3ce5d51e6
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-02-01 13:02:46 +0400

    Add files via upload

---
+++ 1.JS Intro/js/script.js
@@ -0,0 +1,102 @@
+"use strict";
+// alert("Hello P119")
+
+/*
+jagsfjdhf
+sjdhkjdf
+
+askjhj
+
+
+*/
+
+// console.log(5+5)
+
+// let isMaried=true;
+// let $=5;
+// let _="Salam";
+
+// isMaried=5;
+// isMaried="Hello";
+// isMaried=10.5;
+// console.log(isMaried)
+
+// let isMaried=false;
+
+// var number=10;
+
+// var number=100;
+// console.log(number)
+
+// for (let i = 0; i <= 10; i++) {
+
+// }
+
+// console.log(i)
+// var a=5;
+// a=10;
+
+// console.log(a)
+// let x,y,z
+
+// let a=10,
+//     word='H',
+//     isTrue=false
+
+// let person=null;
+
+// console.log(typeof alert)
+
+// let a=10;
+// console.log(a++)
+
+
+// let arr=[10,20,true,"Salam",'']
+// let arr1=[];
+// console.log(arr1[0])
+
+// let age = 19;
+
+// let result=age>18 ? "Xosh gelmishsiniz" : "Get boyuyende gelersen"
+
+// console.log(result)
+
+// if(age>18){
+//     console.log("Xosh gelmishsiniz")
+// }else if(age==18)
+// {
+//     console.log("Aydaaaa az qalmishdi")
+// }
+// else{
+//     console.log("Get boyuyende gelersen")
+// }
+
+// switch (age) {
+//     case 18:
+//     case 16:
+//         console.log("Az qalib")
+//         break;
+//     default:
+//         console.log("Get boyuyende gelersen")
+//         break;
+// }
+
+// for (let index = 0; index <=10; index++) {
+//     console.log(index)
+// }
+
+let i=0
+// while (i<10) {
+//     i++;
+//     // if(i>5 && i<8) continue;
+//     if(i==5) break;
+    
+// }
+
+// console.log(i)
+
+do{
+    console.log(i)
+}while (i<0)
+
+
